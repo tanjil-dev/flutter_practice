4140908e1b2082a3a26e30190b63ae34902487b7
Tanjil Ahmed <tanzil.ovi578@gmail.com>
2021-01-23 16:57:40 +0600

    practice demo code

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+    @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State {
+
+}
